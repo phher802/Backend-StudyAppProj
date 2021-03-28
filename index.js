5388715d255218b79537a2f482c99c5d62ce468a
phher802 <pher024@gmail.com>
2021-03-28 00:40:33 -0500

    Create bare bones of express server

---
+++ index.js
@@ -0,0 +1,15 @@
+//jshint esversion:6
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const apiPort = 3000
+
+// app.use(express.urlendcoded({extended: true}))
+// app.use(cors)
+// app.use(express.json())
+
+// app.get('/', (req, res) => {
+//     res.send("Hello World")
+// })
+
+app.listen(apiPort, () => console.log('server running on port ${apiPort}'))
