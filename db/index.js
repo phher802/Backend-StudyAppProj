a0cf2c7de1f0e16b6e1e195ba86464c53ad57222
phher802 <pher024@gmail.com>
2021-03-28 20:05:04 -0500

    Add db folder with index.js file to use mongoose library

---
+++ db/index.js
@@ -0,0 +1,11 @@
+//jshint esversion:6
+
+const mongoose = require('mongoose');
+
+mongoose
+    .connect('mongodb://localhost:27017/studyDB', {useNewUrlParser: true}, {useUnifiedTopology: true});
+    .catch(e => {console.error('Connection error', e.message)});
+
+const db = mongoose.connection;
+
+module.exports = db;
